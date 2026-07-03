96e1d62b935150aca8ee5531981a3aac38d29e9d
ZiadAmr-10 <ziadddddamr@gmail.com>
2026-07-03 06:40:56 +0300

    Question01

--- Assignment01OOP/Program.cs
+++ Assignment01OOP/Program.cs
@@ -4,7 +4,40 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            #region Part01
+            #region Question01
+            //Class is a Reference type stores in stack(address)+Heap(object),Struct is a value type store in Stack
+            //Class Support inheritance,Struct not support inheritence
+            //Struct Copies the data 
+            // Struct copies the data:
+            //    struct Point
+            //{
+            //    public string Name;
+            //    public int Age;
+            //    public Point(string name, int age)
+            //    {
+            //        Name = name;
+            //        Age = age;
+            //    }
+            //}
+            //Point P01 = new Point ("Ali",10 );
+            //Point P02 = P01;     // Copies the data
+            //P02.Name = "Ahmed";
+            // P01.Name = "Ali"
+            // P02.Name = "Ahmed"
+            //Class Copies the reference 
+            //    class Person
+            //{
+            //    public string Name;
+            //    public int Age;
+            //}
+            //Person P01 = new Person ("Ali",10);
+            //Person P02 = P01;    // Copies the reference (address) not data 
+            //P02.Name = "Ahmed";
+            // P01.Name = "Ahmed"
+            // P02.Name = "Ahmed"
+            #endregion
+            #endregion
     }
+}
 }
